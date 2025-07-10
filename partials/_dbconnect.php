66926fc2db4bff8df6b89a61e6613ad1ff10fc22
NavedHussain <naved1803@gmail.com>
2025-07-10 23:55:46 +0530

    threadlist

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -1,5 +1,16 @@
 <?php
 
   // script to connect to databse
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "idiscuss";
+
+  $conn = mysqli_connect($servername, $username,
+  $password, $database);
 
  ?>
+
+
+
+ 
---
+++ threadlist.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome to iDiscuss - Coding Forums</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+  </head>
+  <body>
+    <?php  include 'partials/_header.php'; ?>
+    <?php  include 'partials/_dbconnect.php'; ?>
+
+
+  <!-- Categories start here -->
+        <div class="container my-4">
+
+          <div class="jumbotron">
+            <h1 class="display-4">Welcome to python forums</h1>
+            <p class="leas">React is awesome but Python is an interpreted, high-level,
+              gerneral-purpose programming language. Python can be used on a server to create web applications.</p>
+            <hr class="my-4">
+          <p> This is a perr to peer forum.No Spam / Advertising / Self-promote in the forums is not allowed.
+              Do not post copyright-infringing material.Do not post “offensive” posts, links or images.
+              Do not cross post questions.Remain respectful of other members at all times.</p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">learn more</a>
+          </div>
+      </div>
+
+      <div class="container">
+    <h1 class="py-2">Browse Question</h1>
+    <div class="d-flex align-items-center my-3">
+    <img src="images.png" width="54px" class="me-3" alt="...">
+      <div>
+        <h5 class="mt-0 mb-1">Unable to install Pyaudio error in Windows </h5>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat.
+      </div>
+    </div>
+
+
+  <div class="d-flex align-items-center media my-3">
+  <img src="images.png" width="54px" class="me-3" alt="...">
+    <div>
+      <h5 class="mt-0 ">Unable to install Pyaudio error in Windows </h5>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat.
+    </div>
+  </div>
+
+  <div class="d-flex align-items-center media my-3">
+  <img src="images.png" width="54px" class="me-3" alt="...">
+    <div>
+      <h5 class="mt-0 ">Unable to install Pyaudio error in Windows </h5>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat.
+    </div>
+  </div>
+
+  <div class="d-flex align-items-center media my-3">
+  <img src="images.png" width="54px" class="me-3" alt="...">
+    <div>
+      <h5 class="mt-0 ">Unable to install Pyaudio error in Windows </h5>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat.
+    </div>
+  </div>
+
+  <div class="d-flex align-items-center media my-3">
+  <img src="images.png" width="54px" class="me-3" alt="...">
+    <div>
+      <h5 class="mt-0 ">Unable to install Pyaudio error in Windows </h5>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat.
+    </div>
+  </div>
+
+  <div class="d-flex align-items-center media my-3">
+  <img src="images.png" width="54px" class="me-3" alt="...">
+    <div>
+      <h5 class="mt-0 ">Unable to install Pyaudio error in Windows </h5>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat.
+    </div>
+  </div>
+
+  <div class="d-flex align-items-center media my-3">
+  <img src="images.png" width="54px" class="me-3" alt="...">
+    <div>
+      <h5 class="mt-0 ">Unable to install Pyaudio error in Windows </h5>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat.
+    </div>
+  </div>
+
+</div>
+
+
+
+    <?php  include 'partials/_footer.php'; ?>
+  <!-- Bootstrap Bundle JS (includes Popper.js) -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.min.js"
+    integrity="sha384-7qAoOXltbVP82dhxHAUje59V5r2YsVfBafyUDxEdApLPmcdhBPg1DKg1ERo0BZlK" crossorigin="anonymous"></script>
+  </body>
+</html>
